fadff84992ce94fc33cdd37bf8164cab4a69f30b
Daniel Streba <danielstreba@gmail.com>
2019-03-16 21:54:19 +0100

    Add Direction & IDirection implementation

---
+++ EvoCarcassonne/Backend/Direction.cs
@@ -0,0 +1,13 @@
+﻿namespace EvoCarcassonne.Backend
+{
+    class Direction : IDirection
+    {
+        private int Id { get; set; }
+
+        private Landscape Landscape { get; set; }
+
+        private Figure Figure { get; set; }
+
+        private IDirection Neighbor { get; set; }
+    }
+}
