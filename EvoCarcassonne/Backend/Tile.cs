b42ccb9a618ffe6210e771034ee5b53a97a79ef8
Daniel Streba <danielstreba@gmail.com>
2019-03-19 20:00:17 +0100

    Fix property naming

--- EvoCarcassonne/Backend/Tile.cs
+++ EvoCarcassonne/Backend/Tile.cs
@@ -5,7 +5,7 @@ namespace EvoCarcassonne.Backend
     public class Tile : ITile
     {
         private List<IDirection> Directions { get; set; }
-        private SpecialProperty SpecialProperty { get; set; }
+        private Speciality Speciality { get; set; }
 
         public void Rotate(IDirection direction)
         {
