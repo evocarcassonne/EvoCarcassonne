65ead11730b5870169e45fa8f4e3256c8daf9423
Daniel Streba <danielstreba@gmail.com>
2019-03-30 13:09:33 +0100

    Add reworked GUI implementation (basic)
    Signed-off-by: Daniel Streba <danielstreba@gmail.com>

--- EvoCarcassonne/MainWindow.xaml.cs
+++ EvoCarcassonne/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EvoCarcassonne
 {
@@ -23,6 +14,37 @@ namespace EvoCarcassonne
         public MainWindow()
         {
             InitializeComponent();
+
+            var tiles = new List<Tile>(100);
+            var random = new Random();
+
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    tiles.Add(new Tile
+                    {
+                        Tag = $"{i + 1},{j + 1}",
+                        Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)))
+                    });
+                }
+            }
+
+            Board.ItemsSource = tiles;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button b)
+            {
+                b.Content = b.Tag.ToString();
+            }
+        }
+    }
+
+    public class Tile
+    {
+        public string Tag { get; set; }
+        public SolidColorBrush Background { get; set; }
     }
 }
