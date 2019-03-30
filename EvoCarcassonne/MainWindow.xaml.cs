872e05e170530e19512740dd5df650e9cca67cbd
Daniel Streba <danielstreba@gmail.com>
2019-03-30 12:59:10 +0100

    Revert "[Do Not Merge] Add basic GUI grid implementation"
    This reverts commit 8b2e4212537db5badfff347b334ea8ad7a665808.

--- EvoCarcassonne/MainWindow.xaml.cs
+++ EvoCarcassonne/MainWindow.xaml.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace EvoCarcassonne
 {
@@ -14,63 +23,6 @@ namespace EvoCarcassonne
         public MainWindow()
         {
             InitializeComponent();
-
-            this.DataContext = new Board(10, 10);
         }
-
-        private void UniformGrid_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            
-        }
-    }
-
-    public class Board
-    {
-        int _rows;
-        int _columns;
-        List<TileF> _tiles = new List<TileF>();
-
-        public Board(int rows, int columns)
-        {
-            _rows = rows;
-            _columns = columns;
-            Random random = new Random();
-
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < columns; c++)
-                {
-                    _tiles.Add(new TileF()
-                    {
-                        Data = string.Format("R{0}C{1}", r, c),
-                        Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)))
-                    });
-                }
-            }
-        }
-
-        public int Rows
-        {
-            get { return _rows; }
-            set { _rows = value; }
-        }
-
-        public int Columns
-        {
-            get { return _columns; }
-            set { _columns = value; }
-        }
-
-        public List<TileF> Tiles
-        {
-            get { return _tiles; }
-            set { _tiles = value; }
-        }
-    }
-
-    public class TileF
-    {
-        public string Data { get; set; }
-        public SolidColorBrush Background { get; set; }
     }
 }
