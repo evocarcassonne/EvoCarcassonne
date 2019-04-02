e9f6c2ccf2a794e13bd336a5874bf7c2507ff8bd
Daniel Streba <danielstreba@gmail.com>
2019-04-02 17:34:39 +0200

    Move Controller logic to MainController
    Signed-off-by: Daniel Streba <danielstreba@gmail.com>

---
+++ EvoCarcassonne/Controller/MainController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace EvoCarcassonne.Controller
+{
+    public class MainController
+    {
+        public MainController()
+        {
+            LoadTiles();
+        }
+
+        public ObservableCollection<Tile> Tiles { get; set; }
+
+        private void LoadTiles()
+        {
+            var tiles = new ObservableCollection<Tile>();
+            var random = new Random();
+
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    tiles.Add(new Tile
+                    {
+                        Tag = $"{i};{j}",
+                        Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256))),
+                    });
+                }
+            }
+
+            Tiles = tiles;
+        }
+
+    }
+
+    public class Tile
+    {
+        public string Tag { get; set; }
+        public SolidColorBrush Background { get; set; }
+
+        public static Tile GetTile(int x, int y, ObservableCollection<Tile> tiles)
+        {
+            if (tiles.Count > x * 10 + y)
+            {
+                return tiles[x * 10 + y];
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
--- EvoCarcassonne/View/MainView.xaml.cs
+++ EvoCarcassonne/View/MainView.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace EvoCarcassonne.View
 {
@@ -15,22 +11,6 @@ namespace EvoCarcassonne.View
         public MainView()
         {
             InitializeComponent();
-
-            var random = new Random();
-
-            for (var i = 0; i < 10; i++)
-            {
-                for (var j = 0; j < 10; j++)
-                {
-                    Tile.Tiles.Add(new Tile
-                    {
-                        Tag = $"{i};{j}",
-                        Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)))
-                    });
-                }
-            }
-
-            Board.ItemsSource = Tile.Tiles;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,27 +18,7 @@ namespace EvoCarcassonne.View
             if (sender is Button b)
             {
                 b.Content = b.Tag.ToString();
-                var coords = b.Tag.ToString().Split(';').Select(int.Parse).ToArray();
-                //MessageBox.Show(coords[0] +" " + coords[1]);
-                var random = new Random();
-
-                b.Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256),
-                    (byte)random.Next(256)));
-                //var x = Tile.GetTile(coords[0], coords[1]);
-                //MessageBox.Show(x.Tag.ToString());
-            }
-        }
-
-        public class Tile
-        {
-            public static readonly List<Tile> Tiles = new List<Tile>(100);
-
-            public string Tag { get; set; }
-            public SolidColorBrush Background { get; set; }
-
-            public static Tile GetTile(int x, int y)
-            {
-                return Tiles[x * 10 + y];
+                // var coords = b.Tag.ToString().Split(';').Select(int.Parse).ToArray();
             }
         }
     }
