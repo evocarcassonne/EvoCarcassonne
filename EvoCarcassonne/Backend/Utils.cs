2f2b431e3beaf6f6f4583788d6f8ef8b55bbd899
ilkukrisz <ilku355@gmail.com>
2019-06-20 13:36:12 +0200

    Implementation of is finished road (#25)
    * IsEndOfRoad is implemented in Utils.
    
    * Some really basic tests added without regards on name conventions.
    
    * Utils updated to store the direction in which a road is finished or not.
    
    * Updated Utils to handle roads that doesn't have an EndOfRoad ending point.
    
    * Kind of refactored. Nice documentation added.
    
    * Faulty file names are corrected to include EndOfRoad specialities.

--- EvoCarcassonne/Backend/Utils.cs
+++ EvoCarcassonne/Backend/Utils.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Animation;
 using EvoCarcassonne.Controller;
 using EvoCarcassonne.Model;
 
@@ -10,7 +12,9 @@ namespace EvoCarcassonne.Backend
 {
     public static class Utils
     {
-        public static int DistanceBetweenTiles = 1;
+        private static BoardTile FirstTile = new BoardTile();
+        private static int DistanceBetweenTiles = 1;
+        public static Dictionary<CardinalDirection, IFigure> FiguresOnBoardTile = new Dictionary<CardinalDirection, IFigure>();
         public static List<string> GetResourceNames(string condition)
         {
             var asm = Assembly.GetEntryAssembly();
@@ -34,7 +38,7 @@ namespace EvoCarcassonne.Backend
             {
                 ILandscape currentTileLandscape = boardTile.BackendTile.getTileSideByCardinalDirection(neighborTile.Key).Landscape;
                 ILandscape neighborTileLandscape = neighborTile.Value.BackendTile
-                    .getTileSideByCardinalDirection(getOppositeDirection(neighborTile.Key)).Landscape;
+                    .getTileSideByCardinalDirection(GetOppositeDirection(neighborTile.Key)).Landscape;
                 
                 if (!currentTileLandscape.Equals(neighborTileLandscape))
                 {
@@ -45,12 +49,6 @@ namespace EvoCarcassonne.Backend
             return true;
         }
 
-
-        public static bool IsFinishedRoad(BoardTile currentTile)
-        {
-            return false;
-        }
-
         public static Dictionary<CardinalDirection, BoardTile> GetSurroundingTiles(BoardTile currentTile)
         {
             Dictionary<CardinalDirection, BoardTile> result =
@@ -58,19 +56,19 @@ namespace EvoCarcassonne.Backend
 
             foreach (var neighborTile in MainController.PlacedBoardTiles)
             {
-                if (isOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,0))
+                if (IsOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,0))
                 {
                     result.Add(CardinalDirection.East, neighborTile);
                 }
-                if (isOnTheGivenSide(currentTile, neighborTile, 0,DistanceBetweenTiles))
+                if (IsOnTheGivenSide(currentTile, neighborTile, 0,DistanceBetweenTiles))
                 {
                     result.Add(CardinalDirection.South, neighborTile);
                 }
-                if (isOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,0))
+                if (IsOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,0))
                 {
                     result.Add(CardinalDirection.West, neighborTile);
                 }
-                if (isOnTheGivenSide(currentTile, neighborTile, 0,-DistanceBetweenTiles))
+                if (IsOnTheGivenSide(currentTile, neighborTile, 0,-DistanceBetweenTiles))
                 {
                     result.Add(CardinalDirection.North, neighborTile);
                 }
@@ -87,10 +85,10 @@ namespace EvoCarcassonne.Backend
             }
             foreach (var neighborTile in MainController.PlacedBoardTiles)
             {
-                if (isOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,DistanceBetweenTiles) ||
-                    isOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,DistanceBetweenTiles) ||
-                    isOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,-DistanceBetweenTiles) ||
-                    isOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,-DistanceBetweenTiles))
+                if (IsOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,DistanceBetweenTiles) ||
+                    IsOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,DistanceBetweenTiles) ||
+                    IsOnTheGivenSide(currentTile, neighborTile, -DistanceBetweenTiles,-DistanceBetweenTiles) ||
+                    IsOnTheGivenSide(currentTile, neighborTile, DistanceBetweenTiles,-DistanceBetweenTiles))
                 {
                     result.Add(neighborTile);
                 }
@@ -98,7 +96,7 @@ namespace EvoCarcassonne.Backend
             return result;
         }
 
-        public static CardinalDirection getOppositeDirection(CardinalDirection direction)
+        public static CardinalDirection GetOppositeDirection(CardinalDirection direction)
         {
             switch (direction)
             {
@@ -110,7 +108,7 @@ namespace EvoCarcassonne.Backend
             }
         }
 
-        public static BoardTile getNeighborTile(Dictionary<CardinalDirection, BoardTile> tilesNextToTheGivenTile, CardinalDirection whereToGo)
+        public static BoardTile GetNeighborTile(Dictionary<CardinalDirection, BoardTile> tilesNextToTheGivenTile, CardinalDirection whereToGo)
         {
             foreach (var pair in tilesNextToTheGivenTile)
             {
@@ -122,10 +120,110 @@ namespace EvoCarcassonne.Backend
             return null;
         }
 
-        public static bool isOnTheGivenSide(BoardTile currentTile, BoardTile neighborTile, int diffX, int diffY)
+        private static bool IsOnTheGivenSide(BoardTile currentTile, BoardTile neighborTile, int diffX, int diffY)
         {
             return currentTile.Coordinates.X + diffX == neighborTile.Coordinates.X &&
                 currentTile.Coordinates.Y + diffY == neighborTile.Coordinates.Y;
+        }
+
+        /// <summary>
+        /// Checks whether a road is finished ot not with the given tile.
+        /// </summary>
+        /// <param name="currentTile"></param>
+        /// <returns>Returns a Dictionary, which tells that whether a road is finished in the given direction. If there is no road on the given tile, it returns an empty dictionary.</returns>
+        public static Dictionary<CardinalDirection, bool> IsFinishedRoad(BoardTile currentTile)
+        {
+            Dictionary<CardinalDirection, bool> result = new Dictionary<CardinalDirection, bool>();
+            for (int i = 0; i < currentTile.BackendTile.Directions.Count; i++)
+            {
+                if (currentTile.BackendTile.Directions[i].Landscape is Road)
+                {
+                    result.Add((CardinalDirection)i, IsRoadFinishedInGivenDirection(currentTile, (CardinalDirection)i, true));
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if the conditions are fulfilled. If not calls itself again until one of the conditions is fulfilled.
+        /// </summary>
+        /// <param name="currentTile">The tile we check</param>
+        /// <param name="whereToGo">The direction we are supposed to expand the checking</param>
+        /// <param name="firstCall">Is is the first call or not</param>
+        /// <returns>A bool which tells whether a road is finished in the given direction</returns>
+        public static bool IsRoadFinishedInGivenDirection(BoardTile currentTile, CardinalDirection whereToGo, bool firstCall)
+        {
+            BoardTile neighborTile = GetNeighborTile(GetSurroundingTiles(currentTile), whereToGo);
+            if (firstCall)
+            {
+                FirstTile = currentTile;
+            }
+            #region Calculation
+            if (neighborTile == null)
+            {
+                return false;
+            }
+            if (IsEndOfRoad(neighborTile) || FirstTile.Coordinates.X == neighborTile.Coordinates.X && FirstTile.Coordinates.Y == neighborTile.Coordinates.Y)
+            {
+                return true;
+            }
+            return SearchInTilesSides(neighborTile, (int)GetOppositeDirection(whereToGo));
+            #endregion
+        }
+        
+        /// <summary>
+        /// Checks whether the given tile is end of a road or not
+        /// </summary>
+        /// <param name="currentTile">The examined tile</param>
+        /// <returns>True if the given tile is end of road, false if not</returns>
+        public static bool IsEndOfRoad(BoardTile currentTile)
+        {
+            return currentTile.BackendTile.Speciality.Contains(Speciality.EndOfRoad);
+        }
+        
+        /// <summary>
+        /// Checks the given tile's side to find roads there, and calls back IsRoadFinishedInGivenDirection
+        /// </summary>
+        /// <param name="neighborTile">The examined tile</param>
+        /// <param name="sideNumber">The index of the side that the road comes from. Aims to avoid infinite loops going back and fourth</param>
+        /// <returns>True or false. If the road is finished then it returns true, else it is false.</returns>
+        private static bool SearchInTilesSides(BoardTile neighborTile, int sideNumber)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (neighborTile.BackendTile.Directions[i].Landscape is Road && i != sideNumber)
+                {
+                    return IsRoadFinishedInGivenDirection(neighborTile, neighborTile.BackendTile.GetCardinalDirectionByIndex(i), false);
+                }
+            }
+            return false;
+        }
+        
+        /// <summary>
+        /// It is supposed to be called after IsFinishedRoad has at least one true value. With the parameters it is going to go, and find the end of road, which is just put down.
+        /// </summary>
+        /// <param name="currentTile">The tile which has just been put down</param>
+        /// <param name="whereToGo">The direction where to search for end of road tile</param>
+        /// <returns>The end of road tile found in the given direction. Null if there is no end of road tile</returns>
+        public static BoardTile SearchEndOfRoadTileInGivenDirection(BoardTile currentTile, CardinalDirection whereToGo)
+        {
+            BoardTile neighborTile = GetNeighborTile(GetSurroundingTiles(currentTile), whereToGo);
+            if (neighborTile == null || IsEndOfRoad(neighborTile))
+            {
+                return neighborTile;
+            }
+            for (var i = 0; i < 4; i++)
+            {
+                if (neighborTile.BackendTile.Directions[i].Figure != null)
+                {
+                    FiguresOnBoardTile.Add((CardinalDirection)i, neighborTile.BackendTile.Directions[i].Figure);
+                }
+                if (neighborTile.BackendTile.Directions[i].Landscape is Road && i != (int)GetOppositeDirection(whereToGo))
+                {
+                    return SearchEndOfRoadTileInGivenDirection(neighborTile, neighborTile.BackendTile.GetCardinalDirectionByIndex(i));
+                }
+            }
+            return null;
         }
        
     }
---
+++ EvoCarcassonneUnitTests/RoadFinishedTests.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using EvoCarcassonne.Backend;
+using EvoCarcassonne.Controller;
+using EvoCarcassonne.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EvoCarcassonneUnitTests
+{
+    [TestClass]
+    public class RoadFinishedTests
+    {
+        
+       private BoardTile boardTile1 = new BoardTile();
+        private BoardTile boardTile2 = new BoardTile();
+        private BoardTile boardTile3 = new BoardTile();
+        private BoardTile boardTile4 = new BoardTile();
+        private BoardTile boardTile5 = new BoardTile();
+        private BoardTile boardTile6 = new BoardTile();
+        private BoardTile boardTile7 = new BoardTile();
+        private BoardTile boardTile8 = new BoardTile();
+        private BoardTile boardTile9 = new BoardTile();
+
+        [TestInitialize]
+        public void Init()
+        {
+            var figure = new Figure(0, new Owner(0, "Krisztian"));
+            
+            boardTile1.Coordinates = new Coordinates(0, 0);
+            boardTile2.Coordinates = new Coordinates(1, 0);
+            boardTile3.Coordinates = new Coordinates(1, 1);
+            boardTile4.Coordinates = new Coordinates(1, 2);
+            boardTile5.Coordinates = new Coordinates(2, 2);
+            boardTile6.Coordinates = new Coordinates(2, 1);
+            boardTile7.Coordinates = new Coordinates(3, 1);
+            boardTile8.Coordinates = new Coordinates(4, 1);
+            boardTile9.Coordinates = new Coordinates(0, 1);
+
+
+            var specialities = new List<Speciality>();
+            specialities.Add(Speciality.EndOfRoad);
+
+            var directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            boardTile1.BackendTile = new Tile(1, directions, specialities);
+
+            specialities = new List<Speciality>();
+            specialities.Add(Speciality.None);
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            boardTile2.BackendTile = new Tile(2, directions, specialities);
+
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            boardTile3.BackendTile = new Tile(3, directions, specialities);
+
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            boardTile4.BackendTile = new Tile(4, directions, specialities);
+
+            specialities = new List<Speciality>();
+            specialities.Add(Speciality.EndOfRoad);
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            boardTile5.BackendTile = new Tile(5, directions, specialities);
+
+            specialities = new List<Speciality>();
+            specialities.Add(Speciality.None);
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            boardTile6.BackendTile = new Tile(6, directions, specialities);
+
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            boardTile7.BackendTile = new Tile(7, directions, specialities);
+
+            specialities = new List<Speciality>();
+            specialities.Add(Speciality.EndOfRoad);
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            boardTile8.BackendTile = new Tile(8, directions, specialities);
+
+            specialities = new List<Speciality>();
+            specialities.Add(Speciality.None);
+            directions = new List<IDirection>();
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Road(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            directions.Add(new Direction(0, new Castle(), figure));
+            boardTile9.BackendTile = new Tile(9, directions, specialities);
+            
+            MainController.PlacedBoardTiles = new ObservableCollection<BoardTile>();
+            MainController.PlacedBoardTiles.Add(boardTile1);
+            MainController.PlacedBoardTiles.Add(boardTile2);
+            MainController.PlacedBoardTiles.Add(boardTile3);
+            MainController.PlacedBoardTiles.Add(boardTile4);
+            MainController.PlacedBoardTiles.Add(boardTile5);
+            MainController.PlacedBoardTiles.Add(boardTile6);
+            MainController.PlacedBoardTiles.Add(boardTile7);
+            MainController.PlacedBoardTiles.Add(boardTile8);
+            MainController.PlacedBoardTiles.Add(boardTile9);
+
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            MainController.PlacedBoardTiles.RemoveAndGet(5);
+            Assert.IsTrue(Utils.IsRoadFinishedInGivenDirection(boardTile6, CardinalDirection.East, true));
+        }
+        
+        [TestMethod]
+        public void Test2()
+        {
+            MainController.PlacedBoardTiles.RemoveAndGet(5);
+            MainController.PlacedBoardTiles.RemoveAndGet(6);
+            Dictionary<CardinalDirection, bool> result = Utils.IsFinishedRoad(boardTile6);
+            bool condition;
+            result.TryGetValue(CardinalDirection.West, out condition);
+            Assert.IsTrue(condition);
+            result.TryGetValue(CardinalDirection.East, out condition);
+            Assert.IsFalse(condition);
+        }
+        
+        [TestMethod]
+        public void Test3()
+        {
+            MainController.PlacedBoardTiles.RemoveAndGet(5);
+            BoardTile resultTile = Utils.SearchEndOfRoadTileInGivenDirection(boardTile6, CardinalDirection.West);
+            Assert.AreEqual(resultTile, boardTile1);
+        }
+        
+    }
+}
