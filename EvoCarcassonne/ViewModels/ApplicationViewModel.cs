e2087bc9a802d8a968f558f5f690a4a59dfcf496
Daniel Streba <danielstreba@gmail.com>
2019-06-25 12:30:47 +0200

    Implement application shell

---
+++ EvoCarcassonne/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace EvoCarcassonne.ViewModels
+{
+    public class ApplicationViewModel : ObservableObject
+    {
+        private IViewModel _currentViewModel;
+        private ObservableCollection<IViewModel> _viewModels;
+
+        public IViewModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                if (_currentViewModel != value)
+                {
+                    _currentViewModel = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<IViewModel> ViewModels
+        {
+            get => _viewModels;
+            set
+            {
+                if (_viewModels != value)
+                {
+                    _viewModels = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ChangeViewModelCommand { get; set; }
+
+        public ApplicationViewModel()
+        {
+            ViewModels = new ObservableCollection<IViewModel>();
+
+            // TODO: add view models to the collection
+
+            CurrentViewModel = ViewModels.First();
+
+            ChangeViewModelCommand = new RelayCommand<string>(ChangeViewModel);
+        }
+
+        private void ChangeViewModel(string viewModelName)
+        {
+            ChangeViewModel(ViewModels.First(vm => vm.Title.Contains(viewModelName)));
+        }
+
+        private void ChangeViewModel(IViewModel viewModel)
+        {
+            if (!ViewModels.Contains(viewModel))
+            {
+                ViewModels.Add(viewModel);
+            }
+
+            CurrentViewModel = ViewModels.FirstOrDefault(vm => vm == viewModel);
+        }
+    }
+}
---
+++ EvoCarcassonne/ViewModels/IViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EvoCarcassonne.ViewModels
+{
+    /// <summary>
+    /// The base view model interface to view model switching in the application shell
+    /// </summary>
+    public interface IViewModel
+    {
+        string Title { get; set; }
+    }
+}
