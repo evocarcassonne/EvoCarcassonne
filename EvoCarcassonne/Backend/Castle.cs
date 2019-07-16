9c536ec4d4759582b95f788e5bbc5d24bdb5a545
Pribeli Tamas <pribeli@gmail.com>
2019-07-16 15:32:36 +0200

    Castle_figure_placing

--- EvoCarcassonne/Backend/Castle.cs
+++ EvoCarcassonne/Backend/Castle.cs
@@ -4,6 +4,7 @@ namespace EvoCarcassonne.Backend
 {
     public class Castle : ILandscape
     {
+        private bool _canPlaceFigure { get; set; }
         public Castle()
         {
         }
@@ -20,7 +21,43 @@ namespace EvoCarcassonne.Backend
 
         public bool CanPlaceFigure(BoardTile currentTile, CardinalDirection whereToGo, Utils utils, bool firstCall)
         {
-            return true;
+            _utils = utils;
+            _boardTilesSize = _utils.GetBoardTilesSize();
+            _placedCastleTiles.Clear();
+            _gameOver = true;
+
+            int result = 0;
+
+            if (CheckEndOfCastle(currentTile) && currentTile.BackendTile.Directions[(int)whereToGo].Landscape is Castle)
+            {
+                _firstCall = true;
+                result += CalculateWithDirections(currentTile, whereToGo);
+
+                if (result > 0 && _figuresOnTiles.Count > 0)
+                    _canPlaceFigure = false;
+                else
+                    _canPlaceFigure = true;
+
+                result = 0;
+
+            }
+            else if (!CheckEndOfCastle(currentTile))
+            {
+
+                _firstCall = true;
+                result += CalculateCastle(currentTile, false);
+
+                if (result > 0 && _figuresOnTiles.Count > 0)
+                    _canPlaceFigure = false;
+                else
+                    _canPlaceFigure = true;
+
+                result = 0;
+
+            }
+
+
+            return _canPlaceFigure;
         }
     }
 }
