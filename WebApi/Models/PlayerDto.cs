56053650011a2de3944a9b50e7a7e6274fc91b7a
Ilku Krisztián <ilku355@gmail.com>
2019-11-16 12:07:53 +0100

    Dto-s added to communicate with outside world.

---
+++ WebApi/Models/PlayerDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class PlayerDto
+    {
+        public Guid PlayerId { get; set; }
+        public int NumberOfFigures { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public PlayerDto(Guid playerId, int numberOfFigures, string name, int points)
+        {
+            PlayerId = playerId;
+            NumberOfFigures = numberOfFigures;
+            Name = name;
+            Points = points;
+        }
+
+        public PlayerDto(int numberOfFigures, string name, int points)
+        {
+            PlayerId = Guid.NewGuid();
+            NumberOfFigures = numberOfFigures;
+            Name = name;
+            Points = points;
+        }
+        
+        public PlayerDto()
+        {
+        }
+
+
+    }
+}
