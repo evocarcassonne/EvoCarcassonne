a6978faddcade06f047ee23f95988b101982e125
ilkukrisztian <ilku355@gmail.com>
2020-01-28 13:59:05 +0100

    Getting new tile returns currenttile

--- DotNetCoreWebApi/Backend/services/impl/GamePlayService.cs
+++ DotNetCoreWebApi/Backend/services/impl/GamePlayService.cs
@@ -72,7 +72,7 @@ namespace DotNetCoreWebApi.Backend.services.impl
         {
             var gamePlay = Controller.GetGamePlayById(gameId);
             var tile = new Tile(new List<IDirection>(), new List<Speciality>());
-            tile.PropertiesAsString = "backtile";
+            tile.PropertiesAsString = gamePlay.CurrentTile.PropertiesAsString;
             if (gamePlay == null || gamePlay.TileStack.Count == 0 || gamePlay.HasCurrentTile || gamePlay.TileIsDown || gamePlay.GameState != GameState.Started)
             {
                 return tile;
